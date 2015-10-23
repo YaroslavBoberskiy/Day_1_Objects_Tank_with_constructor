d7915c8912066d8d164c5b3e4c6a060c8f200e48
Yaroslav <yaroslav.boberskiy@gmail.com>
2015-10-23 22:06:40 +0300

    Initial commit

---
+++ src/Tank.java
@@ -0,0 +1,17 @@
+public class Tank {
+
+    String color;
+    int maxCrew;
+    int maxSpeed;
+
+    Tank() {
+
+    }
+
+    Tank(String color, int maxCrew, int maxSpeed) {
+        this.color = color;
+        this.maxCrew = maxCrew;
+        this.maxSpeed = maxSpeed;
+    }
+
+}
